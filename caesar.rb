28b32f1dd2bf2bb6dec4f5126d24f62f35fc6a1e
Andy Hunt <andy.l.hunt85@gmail.com>
2018-04-20 16:43:01 -0400

    finish Caesar algorithm

--- caesar.rb
+++ caesar.rb
@@ -1,9 +1,10 @@
 def caesar(str, number)
   encoded = []
-  str.split(' ').each do |x|
-    encoded.push(x.gsub(/[A-Za-z]/) { |element| (element.downcase.ord + number) < 123 ?
+  until number < 26 do number -= 26 end
+  str.split(' ').each do |letter|
+    encoded.push(letter.gsub(/[A-Za-z]/) { |element| (element.downcase.ord + number) < 123 ?
       (element.ord + number).chr :
       ((element.ord + number) - 122 + 96).to_i.chr})
   end
-  return encoded.join(' ')
+  encoded.join(' ')
 end
