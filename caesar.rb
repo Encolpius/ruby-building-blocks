9477cbd1495685d28b426a6844a9436f3178da7c
Andy Hunt <andy.l.hunt85@gmail.com>
2018-04-20 22:17:38 -0400

    concise code

--- caesar.rb
+++ caesar.rb
@@ -3,8 +3,7 @@ def caesar_cipher(str, number)
   until number < 26 do number -= 26 end
   str.split(' ').each do |letter|
     encoded.push(letter.gsub(/[A-Za-z]/) { |element| (element.downcase.ord + number) < 123 ?
-      (element.ord + number).chr :
-      ((element.ord + number) - 122 + 96).to_i.chr})
+      (element.ord + number).chr : ((element.ord + number) - 122 + 96).to_i.chr})
   end
   encoded.join(' ')
 end
